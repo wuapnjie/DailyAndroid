ce122cb87f58a0994b2fd8271ddcdc057ccfc40c
wuapnjie <wupanjie0611@gmail.com>
2017-02-06 17:11:10 +0800

    constraint animation

--- ConstrainMaster/app/src/main/java/com/xiaopo/flying/constrainmaster/MainActivity.java
+++ ConstrainMaster/app/src/main/java/com/xiaopo/flying/constrainmaster/MainActivity.java
@@ -1,8 +1,12 @@
 package com.xiaopo.flying.constrainmaster;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.support.constraint.solver.widgets.ConstraintWidget;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.transition.TransitionManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -10,40 +14,27 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+  private ConstraintLayout constraintLayout;
+  private ConstraintSet applyConstraintSet = new ConstraintSet();
+  private ConstraintSet resetConstraintSet = new ConstraintSet();
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
 
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null)
-            .show();
-      }
-    });
+    constraintLayout = (ConstraintLayout) findViewById(R.id.main);
+    resetConstraintSet.clone(constraintLayout);
+    applyConstraintSet.clone(this,R.layout.activity_main2);
   }
 
-  @Override public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
+  public void onApplyClick(View view) {
+    TransitionManager.beginDelayedTransition(constraintLayout);
+
+    applyConstraintSet.applyTo(constraintLayout);
   }
 
-  @Override public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
+  public void onResetClick(View view) {
+    TransitionManager.beginDelayedTransition(constraintLayout);
+    resetConstraintSet.applyTo(constraintLayout);
   }
 }
